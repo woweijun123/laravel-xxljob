77c801eb433ab3cbeca4686fe22a9016ea4b2c6a
weichengjun <1054487195@qq.com>
2025-07-25 18:55:14 +0800

    refactor(xxljob): 重构初始化命令并添加心跳维护功能- 移除了 InitCommand 中的执行器注册逻辑 - 新增 HeartbeatJob 类用于维护执行器心跳 - 在 HeartbeatJob 中实现了执行器注册和心跳维护功能 - 优化了错误处理和日志记录

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -5,34 +5,17 @@ namespace XxlJob\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 use JetBrains\PhpStorm\NoReturn;
-use Throwable;
-use XxlJob\Dispatch\DispatcherApi;
 use XxlJob\Enum\RedisKey;
-use XxlJob\Invoke\XxlJobCalleeCollector;
 
 class InitCommand extends Command
 {
     protected $signature   = 'xxljob:init';
     protected $description = 'XXL-JOB 清除缓存和执行器初始化';
 
-    public function __construct(private readonly DispatcherApi $dispatcherApi)
-    {
-        parent::__construct();
-    }
-
     #[NoReturn] public function handle(): void
     {
         $appName = env('APP_NAME');
         dump(Cache::forget(RedisKey::XxlJob->spr($appName)));
         $this->info('XXL-JOB 清除缓存 成功');
-        try {
-            $executors = array_keys(XxlJobCalleeCollector::getContainer());
-            foreach ($executors as $executor) {
-                $this->dispatcherApi->registry($executor, config('xxljob.executor_uri'));
-            }
-            $this->info('XXL-JOB 执行器注册 成功');
-        } catch (Throwable $e) {
-            $this->error('XXL-JOB 执行器注册 失败：' . $e->getMessage());
-        }
     }
 }
